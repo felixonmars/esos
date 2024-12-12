7c974ae0cfc2223e1f6781e18c7bcba7727b17b8
weijinmei <jinmei.wei@spacemit.com>
2024-12-12 14:38:53 +0800

    k1:ccu&reset: support resets by ccu
    Change-Id: I233f30513db3ca9b7e82086223fb454b2473d7f4

--- bsp/spacemit/drivers/clk/ccu-spacemit-k1x.c
+++ bsp/spacemit/drivers/clk/ccu-spacemit-k1x.c
@@ -367,6 +367,85 @@ static SPACEMIT_CCU_DIV_MUX_GATE(ruart1_clk, "ruart1_clk", ruart1_parent_names,
 		0x6, 0x6, 0x0,
 		0);
 
+static const char *rssp0_parent_names[] = {
+	"pll1_d40_61p44", "pll1_d96_25p6", "pll1_d192_12p8", "vctcxo_3"
+};
+
+static SPACEMIT_CCU_DIV_MUX_GATE(rssp0_clk, "rssp0_clk", rssp0_parent_names,
+		BASE_TYPE_RCPU, RCPU_SSP0_CLK_RST,
+		8, 11, 4, 2,
+		0x6, 0x6, 0x0,
+		0);
+
+//resets
+static SPACEMIT_CCU_GATE_NO_PARENT(rhdmi_audio_rst, "rhdmi_audio_rst", NULL,
+		BASE_TYPE_RCPU, RCPU_HDMI_CLK_RST,
+		BIT(0), BIT(0), 0x0, 0);
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rcan_rst, "rcan_rst", NULL,
+		BASE_TYPE_RCPU, RCPU_CAN_CLK_RST,
+		BIT(0), BIT(0), 0, 0);
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm0_rst, "rpwm0_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM0_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm1_rst, "rpwm1_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM1_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm2_rst, "rpwm2_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM2_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm3_rst, "rpwm3_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM3_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm4_rst, "rpwm4_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM4_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm5_rst, "rpwm5_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM5_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm6_rst, "rpwm6_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM6_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm7_rst, "rpwm7_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM7_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm8_rst, "rpwm8_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM8_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rpwm9_rst, "rpwm9_rst", NULL,
+		BASE_TYPE_RCPU2, RCPU2_PWM9_CLK_RST,
+		BIT(2)|BIT(0), BIT(0), BIT(2), 0 );
+
+static SPACEMIT_CCU_GATE_NO_PARENT(ri2c0_rst, "ri2c0_rst", NULL,
+		BASE_TYPE_RCPU, RCPU_I2C0_CLK_RST,
+		BIT(0), BIT(0), 0, 0);
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rir_rst, "rir_rst", NULL,
+		BASE_TYPE_RCPU, RCPU_IR_CLK_RST,
+		BIT(0), BIT(0), 0, 0);
+
+static SPACEMIT_CCU_GATE_NO_PARENT(ruart0_rst, "ruart0_rst", NULL,
+		BASE_TYPE_RCPU, RCPU_UART0_CLK_RST,
+		BIT(0), BIT(0), 0, 0);
+
+static SPACEMIT_CCU_GATE_NO_PARENT(ruart1_rst, "ruart1_rst", NULL,
+		BASE_TYPE_RCPU, RCPU_UART1_CLK_RST,
+		BIT(0), BIT(0), 0, 0);
+
+static SPACEMIT_CCU_GATE_NO_PARENT(rssp0_rst, "rssp0_rst", NULL,
+		BASE_TYPE_RCPU, RCPU_SSP0_CLK_RST,
+		BIT(0), BIT(0), 0, 0);
+
 static struct clk_hw_onecell_data spacemit_k1x_hw_clks = {
 	.num = CLK_MAX_NO, 
 	.hws = {
@@ -441,6 +520,27 @@ static struct clk_hw_onecell_data spacemit_k1x_hw_clks = {
 		[CLK_RCPU_IR]		= &rir_clk.common.hw,
 		[CLK_RCPU_UART0]	= &ruart0_clk.common.hw,
 		[CLK_RCPU_UART1]	= &ruart1_clk.common.hw,
+		[CLK_RCPU_SSP0] 	= &rssp0_clk.common.hw,
+
+		[CLK_RST_RCPU_HDMIAUDIO]    = &rhdmi_audio_rst.common.hw,
+		[CLK_RST_RCPU_CAN]          = &rcan_rst.common.hw,
+		[CLK_RST_RCPU2_PWM0]        = &rpwm0_rst.common.hw,
+		[CLK_RST_RCPU2_PWM1]        = &rpwm1_rst.common.hw,
+		[CLK_RST_RCPU2_PWM2]        = &rpwm2_rst.common.hw,
+		[CLK_RST_RCPU2_PWM3]        = &rpwm3_rst.common.hw,
+		[CLK_RST_RCPU2_PWM4]        = &rpwm4_rst.common.hw,
+		[CLK_RST_RCPU2_PWM5]        = &rpwm5_rst.common.hw,
+		[CLK_RST_RCPU2_PWM6]        = &rpwm6_rst.common.hw,
+		[CLK_RST_RCPU2_PWM7]        = &rpwm7_rst.common.hw,
+		[CLK_RST_RCPU2_PWM8]        = &rpwm8_rst.common.hw,
+		[CLK_RST_RCPU2_PWM9]        = &rpwm9_rst.common.hw,
+
+		[CLK_RST_RCPU_I2C0]         = &ri2c0_rst.common.hw,
+		[CLK_RST_RCPU_IR]           = &rir_rst.common.hw,
+		[CLK_RST_RCPU_UART0]        = &ruart0_rst.common.hw,
+		[CLK_RST_RCPU_UART1]        = &ruart1_rst.common.hw,
+		[CLK_RST_RCPU_SSP0]         = &rssp0_rst.common.hw,
+
 		[CLK_MAX_NO]		= RT_NULL,
 	},
 };
