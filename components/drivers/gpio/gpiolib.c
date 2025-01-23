3085dd5dfc4e088e717ce0b28fdf6a470e16ddc7
weijinmei <jinmei.wei@spacemit.com>
2025-01-23 15:33:44 +0800

    esos: add bitmap & bitops utility and enable them by default
    Change-Id: I0808eecad309ab786f36a48a54ebed69c1f9c5a9

--- components/drivers/gpio/gpiolib.c
+++ components/drivers/gpio/gpiolib.c
@@ -2,6 +2,7 @@
 #include <rthw.h>
 #include <rtthread.h>
 #include <rtdevice.h>
+#include <rtbitops.h>
 
 #undef ENOSPC
 #define ENOSPC		28	/* No space left on device */
@@ -41,96 +42,6 @@ struct gpio_desc {
 };
 
 static struct gpio_desc gpio_desc[ARCH_NR_GPIOS];
-
-#undef BITS_PER_LONG
-#define BITS_PER_LONG 32
-
-#undef BIT_MASK
-#define BIT_MASK(nr)            (1UL << ((nr) % BITS_PER_LONG))
-
-#undef BIT_WORD
-#define BIT_WORD(nr)            ((nr) / BITS_PER_LONG)
-
-/**
- * test_and_set_bit - Set a bit and return its old value
- * @nr: Bit to set
- * @addr: Address to count from
- *
- * This operation is atomic and cannot be reordered.
- * It may be reordered on other architectures than x86.
- * It also implies a memory barrier.
- */
-static inline int test_and_set_bit(int nr, volatile unsigned long *addr)
-{
- 	unsigned long mask = BIT_MASK(nr);
-	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
-	unsigned long old;
-	unsigned long flags;
-
-	flags = rt_hw_interrupt_disable();
-	old = *p;
-	*p = old | mask;
-	rt_hw_interrupt_enable(flags);
-
-	return (old & mask) != 0;
-}
-
-/**
- * clear_bit - Clears a bit in memory
- * @nr: Bit to clear
- * @addr: Address to start counting from
- *
- * clear_bit() is atomic and may not be reordered.  However, it does
- * not contain a memory barrier, so if it is used for locking purposes,
- * you should call smp_mb__before_clear_bit() and/or smp_mb__after_clear_bit()
- * in order to ensure changes are visible on other processors.
- */
-static inline void clear_bit(int nr, volatile unsigned long *addr)
-{
-	unsigned long mask = BIT_MASK(nr);
-	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
-	unsigned long flags;
-
-	flags = rt_hw_interrupt_disable();
-	*p &= ~mask;
-	rt_hw_interrupt_enable(flags);
-}
-
-/**
- * test_bit - Determine whether a bit is set
- * @nr: bit number to test
- * @addr: Address to start counting from
- */
-static inline int test_bit(int nr, const volatile unsigned long *addr)
-{
-	return 1UL & (addr[BIT_WORD(nr)] >> (nr & (BITS_PER_LONG-1)));
-}
-
-/**
- * set_bit - Atomically set a bit in memory
- * @nr: the bit to set
- * @addr: the address to start counting from
- *
- * This function is atomic and may not be reordered.  See __set_bit()
- * if you do not require the atomic guarantees.
- *
- * Note: there are no guarantees that this function will not be reordered
- * on non x86 architectures, so if you are writing portable code,
- * make sure not to rely on its reordering guarantees.
- *
- * Note that @nr may be almost arbitrarily large; this function is not
- * restricted to acting on a single-word quantity.
- */
-static inline void set_bit(int nr, volatile unsigned long *addr)
-{
-	unsigned long mask = BIT_MASK(nr);
-	unsigned long *p = ((unsigned long *)addr) + BIT_WORD(nr);
-	unsigned long flags;
-
-	flags = rt_hw_interrupt_disable();
-	*p |= mask;
-	rt_hw_interrupt_enable(flags);
-}
 
 static inline void desc_set_label(struct gpio_desc *d, const char *label)
 {
