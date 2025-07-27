81d3b6898caa154616efc34d7f3c71497d5b02b3
Nell <xianbin.zhu@spacemit.com>
2025-07-27 15:48:06 +0800

    k3:rpmi_clk: add a fake clk node for testing purpose
    Signed-off-by: Nell <xianbin.zhu@spacemit.com>
    Change-Id: I1da1be4ac0f9f88854538163cd9a0a814f23dc45

--- bsp/spacemit/drivers/rpmi/k3/k3-os0_clk.c
+++ bsp/spacemit/drivers/rpmi/k3/k3-os0_clk.c
@@ -1,11 +1,22 @@
 #include <rthw.h>
 #include <rtthread.h>
 #include <dtb_head.h>
+#include <librpmi.h>
 #include "../spacemit-rpmi.h"
+
+static rpmi_uint64_t rproc_clk[] = {500000000, 900000000, 50000000};
+
+static rpmi_uint64_t rproc_new_rate = 500000000;
+static enum rpmi_clock_state rproc_state = RPMI_CLK_STATE_ENABLED;
 
 struct rpmi_clock_data k3_os0_clk_data[1] = {
 	[0] = {
-		. parent_id = -1,
+		.parent_id = -1,
+		.transition_latency_ms = 5,
+		.clock_type = RPMI_CLK_TYPE_LINEAR,
+		.name = "rproc_clk",
+		.rate_count = 9,
+		.clock_rate_array = rproc_clk,
 	},
 };
 
@@ -23,28 +34,37 @@ static rt_int32_t  _k3_os0_clock_init(void *priv)
 
 static enum rpmi_error spacemit_set_state(void *priv, rpmi_uint32_t clock_id, enum rpmi_clock_state state)
 {
+	rproc_state = state;
+
 	return 0;
 }
 
 static enum rpmi_error spacemit_get_state_and_rate(void *priv, rpmi_uint32_t clock_id, enum rpmi_clock_state *state, rpmi_uint64_t *rate)
 {
-	*state = RPMI_CLK_STATE_ENABLED;
+	*state = rproc_state;
+	*rate = rproc_new_rate;
 
 	return 0;
 }
 
 static rpmi_bool_t spacemit_rate_change_match(void *priv, rpmi_uint32_t clock_id, rpmi_uint64_t rate)
 {
+	rproc_new_rate = rate;
+
 	return 0;
 }
 
 static enum rpmi_error spacemit_set_rate(void *priv, rpmi_uint32_t clock_id, enum rpmi_clock_rate_match match, rpmi_uint64_t rate, rpmi_uint64_t *new_rate)
 {
+	rproc_new_rate = *new_rate;
+
 	return 0;
 }
 
 static enum rpmi_error spacemit_set_rate_recalc(void *priv, rpmi_uint32_t clock_id, rpmi_uint64_t parent_rate, rpmi_uint64_t *new_rate)
 {
+	rproc_new_rate = *new_rate;
+
 	return 0;
 }
 
