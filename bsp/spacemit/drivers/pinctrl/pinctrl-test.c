30c4a53ef9036044c384d755980bf31a990a61a4
Troy Mitchell <zexi.guo@spacemit.com>
2026-01-15 19:45:52 +0800

    pinctrl: test: support test any pin
    Change-Id: Ia263d2fa9097c04acb1bf7ee26b37acaa7eec7fe
    Signed-off-by: Troy Mitchell <zexi.guo@spacemit.com>

--- bsp/spacemit/drivers/pinctrl/pinctrl-test.c
+++ bsp/spacemit/drivers/pinctrl/pinctrl-test.c
@@ -1,11 +1,14 @@
 #include <rtthread.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef enum {
 	PMUX_PULL_DIS,
 	PMUX_PULL_DOWN,
 	PMUX_PULL_UP,
 }pull_type_t;
+
+#define PMUX_BASE_ADDR			0xd401e000
 
 #define PMUX_BIT(x)			(1ul << x)
 
@@ -22,6 +25,13 @@ typedef enum {
 	rt_kprintf("[%s:%d %s] " fmt"\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 
 #ifdef RT_USING_FINSH
+
+static inline uint32_t _pin2offset(uint16_t pin)
+{
+	if (pin > 130)
+		pin += 2;
+	return pin << 2;
+}
 
 static inline uint32_t read_reg(uintptr_t addr)
 {
@@ -103,20 +113,54 @@ static int pmux_check_uart0(void)
 	       check_all(rx_val, PMUX_PULL_UP, 4, 8);
 }
 
-static int pmux_test(void)
+static void pmux_print_usage(void)
 {
-	int ret;
+	rt_kprintf("Usage: pmux_check_pin <pin> <pull> <mode> <strength>\n");
+	rt_kprintf("  pull: 0=DIS, 1=DOWN, 2=UP\n");
+	rt_kprintf("  mode: 0-7\n");
+	rt_kprintf("  strength: 0-15\n");
+}
 
-	ret = pmux_check_uart0();
-	if (ret) {
-		rt_kprintf("%s:%d, check_uart0 failed\n", __func__, __LINE__);
-		return -RT_EINVAL;
+static void pmux_test(int argc, char **argv)
+{
+	int pin, ret, pull_in, mode_in, strength_in;
+	uint32_t val;
+	uint32_t reg_addr;
+
+	if (argc == 1) {
+		rt_kprintf("No arguments, fall back to check uart0\n");
+
+		ret = pmux_check_uart0();
+		if (!ret)
+			return;
+
+		rt_kprintf("ok\n");
+		return;
 	}
 
-	rt_kprintf("ok\n");
-	return 0;
+	if (argv[1][0] >= '0' && argv[1][0] <= '9') {
+		if (argc < 5) {
+			pmux_print_usage();
+			return;
+		}
+
+		pin = atoi(argv[1]);
+		pull_in = atoi(argv[2]);
+		mode_in = atoi(argv[3]);
+		strength_in = atoi(argv[4]);
+
+		reg_addr = PMUX_BASE_ADDR + _pin2offset((uint16_t)pin);
+		val = read_reg(reg_addr);
+
+		if (!check_all(val, (pull_type_t)pull_in, (uint32_t)mode_in, (uint32_t)strength_in))
+			return;
+
+		rt_kprintf("ok\n");
+	} else {
+		pmux_print_usage();
+	}
 }
 
 #include <finsh.h>
-MSH_CMD_EXPORT(pmux_test, pinctrl driver test. e.g: pmux_test());
+MSH_CMD_EXPORT(pmux_test, pinctrl driver test. e.g: pmux_test help);
 #endif
