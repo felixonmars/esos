570c95d33a17211db69b97e25364ed57d6fa4530
liulixin <lixin.liu@spacemit.com>
2026-04-30 10:18:22 +0800

    Update test interrupt macro definition
    Change-Id: I2af92dd99d657f7d8f67a45aa42511edcc555b16
    Signed-off-by: liulixin <lixin.liu@spacemit.com>

--- libcpu/risc-v/spacemit/rt24/riscv-system-init.c
+++ libcpu/risc-v/spacemit/rt24/riscv-system-init.c
@@ -10,14 +10,66 @@
 
 static volatile unsigned long tick_cycles = 0;
 
+#if defined(BSP_TEST_RT) && defined(RT_USING_FINSH)
+
+#define RT_TEST_NUMBER  500
+volatile rt_uint64_t tick_last_cmp = 0;
+volatile unsigned int tick_latency_count = 0;
+volatile unsigned int tick_latency_max_count = 0;
+unsigned long long tick_latency_buf[RT_TEST_NUMBER];
+
+static int rt_tick_latency(void)
+{
+	int i;
+	unsigned long long all = 0, lat_min = (unsigned long long)-1, lat_max = 0;
+
+	tick_latency_count = 0;
+	tick_latency_max_count = RT_TEST_NUMBER;
+
+	rt_kprintf("Collecting %d tick latency samples...\n", RT_TEST_NUMBER);
+
+	while (tick_latency_count < RT_TEST_NUMBER)
+		rt_thread_delay(10);
+
+	tick_latency_max_count = 0;
+
+	for (i = 0; i < RT_TEST_NUMBER; i++) {
+		all += tick_latency_buf[i];
+		if (tick_latency_buf[i] < lat_min) lat_min = tick_latency_buf[i];
+		if (tick_latency_buf[i] > lat_max) lat_max = tick_latency_buf[i];
+	}
+
+	rt_kprintf("Tick IRQ latency: avg=%lldns, min=%lldns, max=%lldns\n",
+		all * 1000000000ULL / SOC_TIMER_FREQ / RT_TEST_NUMBER,
+		lat_min * 1000000000ULL / SOC_TIMER_FREQ,
+		lat_max * 1000000000ULL / SOC_TIMER_FREQ);
+
+	return 0;
+}
+
+MSH_CMD_EXPORT(rt_tick_latency, "measure tick interrupt latency");
+
+#endif
+
 void rt_hw_tick_isr(void)
 {
 	rt_uint64_t value;
 
-	rt_tick_increase();
+#if defined(BSP_TEST_RT) && defined(RT_USING_FINSH)
 
+	if (tick_last_cmp != 0 && tick_latency_count < tick_latency_max_count) {
+		tick_latency_buf[tick_latency_count] = SysTimer_GetLoadValue() - tick_last_cmp;
+		tick_latency_count++;
+	}
+#endif
+
+	rt_tick_increase();
 	value = SysTimer_GetLoadValue() + tick_cycles;
 	SysTimer_SetCompareValue(value);
+
+#if defined(BSP_TEST_RT) && defined(RT_USING_FINSH)
+	tick_last_cmp = value;
+#endif
 }
 
 /**
